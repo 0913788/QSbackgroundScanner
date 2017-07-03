204d67d980faccc0c283a9c20440d6f81ccf9dca
ricardo <ricardo@mail.as2>
2017-07-03 14:10:39 +0200

    initial

---
+++ ScannerSkin/ScannerSkin/Code/DbCall.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ScannerSkin.Code
+{
+    class DbCall
+    {
+        public void Query()
+        {
+            DataTable table = new DataTable();
+            SqlConnection Connection = new SqlConnection("Data Source=145.24.222.155,8080;Database=KickstartRotterdam;Persist Security Info=True;User ID=KickstartRotterdam;Password=Pr0ject3");
+            Connection.Open();
+
+            SqlCommand Command = new SqlCommand("SELECT month(cast(TheftDate as DATE)), convert(char(3), TheftDate, 0) as Months, count(TheftID) as total from BicycleTheft group by month(cast(TheftDate as DATE)),convert(char(3), TheftDate, 0)order by month(cast(TheftDate as DATE))", Connection);
+            using (SqlDataAdapter Data = new SqlDataAdapter(Command)) { Data.Fill(table); }
+            foreach (DataRow row in table.Rows)
+            {
+
+                Main_Chart.Series["Bicycle thefts"].Points.AddXY(row["Months"], row["total"]);
+            }
+            Connection.Close();
+        }
+    }
+}
---
+++ ScannerSkin/ScannerSkin/Code/ProbeAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerSkin.Code
+{
+    class ProbeAdapter
+    {
+        bool ActiveDevice = false;
+
+        public ProbeAdapter()
+        {
+            SetWifiDevice();
+        }
+
+        private async void SetWifiDevice()
+        {
+            var access = await WiFiAdapter.RequestAccessAsync();
+            if (access != WiFiAccessStatus.Allowed)
+            {
+                throw new Exception("WiFiAccessStatus not allowed");
+            }
+            else
+            {
+                var WifiDeviceScan = await WiFiAdapter.FindAllAdaptersAsync();
+                if (WifiDeviceScan.Count >= 1)
+                {
+                    WifiDevice = WifiDeviceScan[0];
+                    ActiveDevice = true;
+                }
+            }
+        }
+        private async void Scan()
+        {
+            if (ActiveDevice) await WifiDevice.ScanAsync();
+            else return;
+        }
+    }
+}
