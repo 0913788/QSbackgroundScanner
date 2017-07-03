c3d48f0ba4301415101746b50ee876e377661b3d
ricardo <ricardo@mail.as2>
2017-07-03 14:22:59 +0200

    db update

--- ScannerSkin/ScannerSkin/Code/DbCall.cs
+++ ScannerSkin/ScannerSkin/Code/DbCall.cs
@@ -1,29 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data;
-using System.Data.SqlClient;
+
 
 namespace ScannerSkin.Code
 {
     class DbCall
     {
-        public void Query()
+        public async void PostLocation(int empID, float x, float y)
         {
-            DataTable table = new DataTable();
-            SqlConnection Connection = new SqlConnection("Data Source=145.24.222.155,8080;Database=KickstartRotterdam;Persist Security Info=True;User ID=KickstartRotterdam;Password=Pr0ject3");
-            Connection.Open();
-
-            SqlCommand Command = new SqlCommand("SELECT month(cast(TheftDate as DATE)), convert(char(3), TheftDate, 0) as Months, count(TheftID) as total from BicycleTheft group by month(cast(TheftDate as DATE)),convert(char(3), TheftDate, 0)order by month(cast(TheftDate as DATE))", Connection);
-            using (SqlDataAdapter Data = new SqlDataAdapter(Command)) { Data.Fill(table); }
-            foreach (DataRow row in table.Rows)
+            using (var client = new HttpClient())
             {
-
-                Main_Chart.Series["Bicycle thefts"].Points.AddXY(row["Months"], row["total"]);
+                var response =
+                    await client.GetStringAsync(string.Format("http://145.24.222.153/Webserver/NewPosition/?empId={}&X={}&Y={}",empID, x, y));
             }
-            Connection.Close();
         }
     }
 }
